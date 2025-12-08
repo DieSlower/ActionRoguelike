738e45c7a37419f39a783bca344b4566422fea2f
DieSlower <maximster@gmail.com>
2025-12-07 23:29:07 -0500

    Adds player character camera system
    Initializes a spring arm and camera component to provide a default follow-camera setup for the player character.
    
    Configures the editor to load a specific test map on startup for development convenience.

--- Source/ActionRoguelike/RogueChartecter.h
+++ Source/ActionRoguelike/RogueChartecter.h
@@ -6,6 +6,9 @@
 #include "GameFramework/Character.h"
 #include "RogueChartecter.generated.h"
 
+class USpringArmComponent;
+class UCameraComponent;
+
 UCLASS()
 class ACTIONROGUELIKE_API ARogueChartecter : public ACharacter
 {
@@ -16,6 +19,14 @@ public:
 	ARogueChartecter();
 
 protected:
+	
+	UPROPERTY(VisibleAnywhere, Category="Components")
+	TObjectPtr<UCameraComponent> CameraComponent;
+	
+	UPROPERTY(VisibleAnywhere, Category="Components")
+	TObjectPtr<USpringArmComponent> SpringArmComponent;
+	
+	
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
