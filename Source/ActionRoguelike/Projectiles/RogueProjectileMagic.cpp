0935eae618b8102120c248c2d1ae20fd3fe27247
DieSlower <maximster@gmail.com>
2025-12-14 19:26:10 -0500

    Adds magic projectile collision and damage
    Implements collision detection for the magic projectile, binding hit events in PostInitializeComponents. The projectile now applies damage to actors it collides with and spawns a configurable explosion effect before being destroyed.
    
    Exposes an 'ExplosionEffect' property to allow blueprint customization of the impact visual effects.

--- Source/ActionRoguelike/Projectiles/RogueProjectileMagic.cpp
+++ Source/ActionRoguelike/Projectiles/RogueProjectileMagic.cpp
@@ -4,9 +4,10 @@
 #include "RogueProjectileMagic.h"
 
 #include "NiagaraComponent.h"
+#include "NiagaraFunctionLibrary.h"
 #include "Components/SphereComponent.h"
 #include "GameFramework/ProjectileMovementComponent.h"
-
+#include "Kismet/GameplayStatics.h"
 
 // Sets default values
 ARogueProjectileMagic::ARogueProjectileMagic()
@@ -19,14 +20,28 @@ ARogueProjectileMagic::ARogueProjectileMagic()
 	
 	LoopedNiagaraComponent = CreateDefaultSubobject<UNiagaraComponent>(TEXT("LoopedNiagaraComp"));
 	LoopedNiagaraComponent->SetupAttachment(SphereComponent);
-	
-	
+		
 	ProjectileMovementComponent = CreateDefaultSubobject<UProjectileMovementComponent>(TEXT("ProjectileMoveComp"));
 	ProjectileMovementComponent->InitialSpeed=2000.f;
 	ProjectileMovementComponent->ProjectileGravityScale=0.f;
-	
-	
-	
 }
 
+void ARogueProjectileMagic::PostInitializeComponents()
+{
+	Super::PostInitializeComponents();
+	
+	SphereComponent->OnComponentHit.AddDynamic(this, &ARogueProjectileMagic::OnActorHit);
+}
 
+void ARogueProjectileMagic::OnActorHit(UPrimitiveComponent* HitComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit )
+{
+	//@todo Create our own damage type
+	TSubclassOf<UDamageType> DamageTypeClass = UDamageType::StaticClass();
+	
+	UGameplayStatics::ApplyDamage(OtherActor, 10.f, GetInstigatorController(), this, DamageTypeClass);
+	
+	
+	UNiagaraFunctionLibrary::SpawnSystemAtLocation(this, ExplosionEffect, GetActorLocation());
+	
+	Destroy();
+}
--- Source/ActionRoguelike/RogueChartecter.cpp
+++ Source/ActionRoguelike/RogueChartecter.cpp
@@ -16,7 +16,6 @@ ARogueChartecter::ARogueChartecter()
  	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 	
-	
 	SpringArmComponent = CreateDefaultSubobject<USpringArmComponent>(TEXT("SpringArmComp"));
 	SpringArmComponent->SetupAttachment(RootComponent);
 	SpringArmComponent->bUsePawnControlRotation = true;
