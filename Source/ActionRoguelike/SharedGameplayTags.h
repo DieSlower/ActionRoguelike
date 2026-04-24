e81a0e0468c8cfc7b95c794cd6d658ae804fa58b
DieSlower <maximster@gmail.com>
2026-04-24 17:16:01 -0400

    Issue main: Implement base animation instance and expose sprinting status
    Introduces `URogueAnimInstance` to provide a central location for animation logic.
    This instance now reads the `StatusEffect.Sprinting` gameplay tag from the
    Action System Component to expose a `bIsSprinting` boolean, enabling animation
    blueprints to react to the sprinting status.
    
    Also adds new core GameplayTags for damage types, general status effects, and
    attributes to `DefaultGameplayTags.ini`, and refines `StopAction` with a safety
    check to prevent stopping already inactive actions.

--- Source/ActionRoguelike/SharedGameplayTags.h
+++ Source/ActionRoguelike/SharedGameplayTags.h
@@ -12,4 +12,7 @@ namespace SharedGameplayTags
 	UE_DECLARE_GAMEPLAY_TAG_EXTERN(Action_SecondaryAttack)
 	UE_DECLARE_GAMEPLAY_TAG_EXTERN(Action_SpecialAttack)
 	UE_DECLARE_GAMEPLAY_TAG_EXTERN(Action_Sprint)
+	
+	UE_DECLARE_GAMEPLAY_TAG_EXTERN(StatusEffect_Sprinting)
+
 }
