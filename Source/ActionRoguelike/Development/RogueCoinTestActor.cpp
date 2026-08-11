23bf8e182efdaf8298dcaff38cc0940523a6ee85
DieSlower <maximster@gmail.com>
2026-08-10 23:06:26 -0400

    Issue main: Implement foundational coin pickup system with spawning and management
    *   Introduces `URogueCoinPickupSubsystem` to manage coin pickup locations and amounts globally.
    *   Adds `ARogueCoinTestActor` for development-time spawning of coins, leveraging the `NavigationSystem` to find valid points on the navigation mesh.
    *   Currently visualizes coin locations using debug points in the world.

---
+++ Source/ActionRoguelike/Development/RogueCoinTestActor.cpp
@@ -0,0 +1,41 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "RogueCoinTestActor.h"
+
+#include "NavigationSystem.h"
+#include "Pickups/RogueCoinPickupSubsystem.h"
+
+ARogueCoinTestActor::ARogueCoinTestActor()
+{
+	DefaultSceneComp = CreateDefaultSubobject<USceneComponent>(TEXT("DefaultSceneComponent"));
+	RootComponent = DefaultSceneComp;
+}
+
+void ARogueCoinTestActor::SpawnCoins(int32 SpawnCount)
+{
+	
+	TArray<FVector> CoinLocations;
+	TArray<int32> CoinAmounts;
+	
+	UNavigationSystemV1* NavSystem = UNavigationSystemV1::GetNavigationSystem(this);
+	FVector ActorLocation = GetActorLocation();
+	
+	for (int i=0; i<SpawnCount; ++i)
+	{
+		// Generate a random point that is on a navigation mesh for our use
+		FNavLocation NavLocation;
+		NavSystem->GetRandomPointInNavigableRadius(ActorLocation, 1024, NavLocation);
+		
+		//Add the new point and it's coin amount to our data arrays. 
+		CoinLocations.Add(NavLocation);
+		CoinAmounts.Add(10);	
+		
+	}
+	
+	URogueCoinPickupSubsystem* CoinSystem = GetWorld()->GetSubsystem<URogueCoinPickupSubsystem>();
+	
+	CoinSystem->AddCoinPickups(CoinLocations, CoinAmounts);
+}
+
+
---
+++ Source/ActionRoguelike/Pickups/RogueCoinPickupSubsystem.h
@@ -0,0 +1,30 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Subsystems/WorldSubsystem.h"
+#include "RogueCoinPickupSubsystem.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ACTIONROGUELIKE_API URogueCoinPickupSubsystem : public UTickableWorldSubsystem
+{
+	GENERATED_BODY()
+	
+public:
+	
+	void AddCoinPickups(TArray<FVector> NewLocations, TArray<int32> NewAmounts);
+	
+	virtual void Tick(float DeltaTime) override;
+	
+	virtual TStatId GetStatId() const override;
+	
+protected:
+	
+	TArray<FVector> CoinLocations;
+	TArray<int32> CoinAmounts;
+	
+};
